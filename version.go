63f5436e22896fae3ab2f185c17423e277d54153
Averlex <averlex2809@gmail.com>
2025-08-19 15:40:12 +0300

    helpers error handling enhanced

--- version.go
+++ version.go
@@ -9,6 +9,9 @@ import (
 // PlainVersionPrinter returns a function that prints version as plain text.
 func PlainVersionPrinter(version string) func(io.Writer) error {
 	return func(w io.Writer) error {
+		if w == nil {
+			return fmt.Errorf("nil writer received")
+		}
 		_, err := fmt.Fprintln(w, version)
 		return err
 	}
@@ -23,6 +26,9 @@ func JSONVersionPrinter(version, commit, date string) func(io.Writer) error {
 	}
 	data := info{Version: version, Commit: commit, Date: date}
 	return func(w io.Writer) error {
+		if w == nil {
+			return fmt.Errorf("nil writer received")
+		}
 		return json.NewEncoder(w).Encode(data)
 	}
 }
